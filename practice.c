23d0c437813eab307755191712d69579ad943c9f
shohir0 <shoo.hirobe@gmail.com>
2025-08-10 22:00:55 +0900

    C language

---
+++ practice.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(int arg, char** argv){
+    
+    printf("---1日目---\n");
+    printf("廣部翔\n");
+    printf("123\n456\n789\n");
+
+    printf("---2日目---\n");
+
+    
+}
