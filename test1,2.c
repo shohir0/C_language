5ac0530db5d7384a01203797520567f78146345b
ShoHir0 <shoo.hirobe@gmail.com>
2025-08-19 19:28:45 +0900

    学びました

--- test1,2.c
+++ test1,2.c
@@ -2,10 +2,11 @@
 #include <stdio.h>
 // ヘッダファイルの宣言
 
-int main(int argc, char** argv){
+int main(int argc, char **argv)
+{
     // main関数の定義
 
-printf("---1日目---\n");
+    printf("---1日目---\n");
     printf("Hello, world.\n");
     // コンソール画面に表示する関数
     // ;←分の終わりに使う、複数の処理が圧場合処理を区切れる
@@ -13,35 +14,35 @@ printf("---1日目---\n");
     printf("123\n456\n789\n");
     printf("シングルクオーテーション: \' ダブルクオーテーション: \"");
 
-    printf("私に名前は%sです。 年齢は%dです。/n","廣部ちゃん",21);
-    printf("私のイニシャルは%Cです。\n",'H');
+    printf("私に名前は%sです。 年齢は%dです。/n", "廣部ちゃん", 21);
+    printf("私のイニシャルは%Cです。\n", 'H');
     printf("%1f + %1f = %1f\n", 1.0, 2.0, 1.0 + 2.0);
-/*--------------------------------------------------------------------*/
+    /*--------------------------------------------------------------------*/
     printf("---2日目---\n");
 
     /*演算子を用いた計算*/
 
-    printf("%d + %d = %d\n",5,2,5+2);
-    printf("%d - %d = %d\n",5,2,5-2);
-    printf("%d * %d = %d\n",5,2,5*2);
-    printf("%d / %d = %d\n",5,2,5/2);
+    printf("%d + %d = %d\n", 5, 2, 5 + 2);
+    printf("%d - %d = %d\n", 5, 2, 5 - 2);
+    printf("%d * %d = %d\n", 5, 2, 5 * 2);
+    printf("%d / %d = %d\n", 5, 2, 5 / 2);
 
-    int a;         // 変数の宣言
-    int b = 3;     // 初期化と代入を同時に行う　ここは定数？
-   unsigned int add,sub;   // int以外の変数を宣言
-    double avg;    // 小数点を表現できる　floutよりもイイ！
-    
+    int a;                 // 変数の宣言
+    int b = 3;             // 初期化と代入を同時に行う　ここは定数？
+    unsigned int add, sub; // int以外の変数を宣言
+    double avg;            // 小数点を表現できる　floutよりもイイ！
+
     a = 6;
-    add = a + b; 
+    add = a + b;
     sub = a - b;
-    avg = (a + b) / 2.0;  //()は優先順位
+    avg = (a + b) / 2.0; //()は優先順位
 
-    printf("%d + %d = %d\n",a,b,add);
-    printf("%d - %d = %d\n",a,b,sub);
-    printf("%dと%dの平均値：%f\n",a,b,avg);
+    printf("%d + %d = %d\n", a, b, add);
+    printf("%d - %d = %d\n", a, b, sub);
+    printf("%dと%dの平均値：%f\n", a, b, avg);
 
-    int i1,i2,j1,j2;
-    double d1,d2,e1,e2;
+    int i1, i2, j1, j2;
+    double d1, d2, e1, e2;
 
     j1 = 3;
     j2 = 3;
@@ -49,19 +50,17 @@ printf("---1日目---\n");
     d1 = 1.23;
     d2 = 1.23;
 
-    i1 = d1; 
-    i2 = (int)d2; 
+    i1 = d1;
+    i2 = (int)d2;
 
     e1 = j1;
     e2 = (double)j2;
 
-    printf("d1 = %f d2 = %f\n",d1,d2);
-    printf("i1 = %d i2 = %d\n",i1,i2);
-    printf("j1 = %d j2 = %d\n",j1,j2);
-    printf("e1 = %f e2 = %f\n",e1,e2);
+    printf("d1 = %f d2 = %f\n", d1, d2);
+    printf("i1 = %d i2 = %d\n", i1, i2);
+    printf("j1 = %d j2 = %d\n", j1, j2);
+    printf("e1 = %f e2 = %f\n", e1, e2);
 
-
-    
     return 0;
     // プログラムの終了を示す
 }
@@ -69,4 +68,4 @@ printf("---1日目---\n");
 
 ・コード内で同じ書式設定に複数の実数を定義できるのか
 
-*/ 
+*/
--- test3.c
+++ test3.c
@@ -1,87 +1,94 @@
 #include <locale.h>
 #include <stdio.h>
 
-int main(int arg,char**argv){
+int main(int arg, char **argv)
+{
 
     int a;
     printf("数値を入力してね：");
 
-    scanf("%d",&a);
+    scanf("%d", &a);
 
-    if(a>0){
+    if (a > 0)
+    {
         printf("入力した値は正の数です。\n");
-    }else{
+    }
+    else
+    {
         printf("入力した値は、正の数ではありません\n");
-
     }
 
     int num;
     printf("1から3の数字を入れてね：");
 
-    scanf("%d",&num);
+    scanf("%d", &num);
 
-    if(num == 1){
+    if (num == 1)
+    {
         printf("one\n");
-    }else if(num == 2){
+    }
+    else if (num == 2)
+    {
         printf("two\n");
-    }else if(num == 3){
-                printf("three\n");
-            }else{
-                printf("不適切な値です\n");
+    }
+    else if (num == 3)
+    {
+        printf("three\n");
+    }
+    else
+    {
+        printf("不適切な値です\n");
+    }
 
-            }
+    int dice;
 
-            int dice;
+    printf("さいころの値を入力してね：");
+    scanf("%d", &dice);
 
-            printf("さいころの値を入力してね：");
-            scanf("%d",&dice);
+    if (1 <= dice && dice <= 6)
+    {
 
-if(1 <= dice && dice <= 6){
+        if (dice == 1 || dice == 3 || dice == 5)
+        {
 
-            if(dice == 1||dice == 3||dice == 5){
+            printf("半！\n");
+        }
+        else if (dice == 2 || dice == 4 || dice == 6)
+        {
 
-                printf("半！\n");
+            printf("丁！\n");
+        }
+    }
+    else
+    {
 
-            }else if(dice == 2||dice == 4||dice == 6){
+        printf("範囲外の数値です\n");
+    }
 
-                printf("丁！\n");
-            }
+    int i;
+    printf("1から3の数を入力してください：");
 
-            }else{
+    scanf("%d", &i);
 
-                printf("範囲外の数値です\n");
-            }
+    switch (i)
+    {
 
-            int i;
-            printf("1から3の数を入力してください：");
+    case 1:
+        printf("one\n");
+        break;
 
-            scanf("%d",&i);
+    case 2:
+        printf("two");
+        break;
 
-            switch(i){
+    case 3:
+        printf("three\n");
+        break;
 
-                case 1:
-                printf("one\n");
-                break;
+    default:
+        printf("不適切な値です\n");
+        break;
+    }
 
-                case 2:
-                printf("two");
-                break;
-
-                case 3:
-                printf("three\n");
-                break;
-
-                default:
-                printf("不適切な値です\n");
-                break;
-                
-            }
-            
-
-
-
-
-
-
- return 0;   
+    return 0;
 }
