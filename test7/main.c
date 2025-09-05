44a8392488ea6b2ceb9b4831a19af95409031523
ShoHir0 <shoo.hirobe@gmail.com>
2025-09-06 02:27:09 +0900

    変更しました

--- test7/main.c
+++ test7/main.c
@@ -7,7 +7,7 @@ int main(int argc, char** argv) {
     //	同じ計算が3回(関数を呼び出して計算）
     d1 = avg(a,b);
     d2 = avg(4.1,5.7);
-    d3 = avg(c,2.8);
+    d3 = avg(c,2.8);  /*dsdsdsdsds*/
     printf("d1 = %lf,d2 = %lf,d3 = %lf\n",d1,d2,d3);
     return 0;
 }
